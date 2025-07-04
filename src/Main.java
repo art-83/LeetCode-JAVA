4fc90b81ea8380a5f2a7bacda94cd09267ffa544
art-83 <arthurfmedeiros77@gmail.com>
2025-07-04 19:47:27 -0300

    feat: +1480

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import com.leetcode.solution.Solution;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.printf("%d", s.easy.maxCount(3,3, new int[][] {{2,2}, {3,3}}));
+        BigDecimal a = new BigDecimal("0.1");
+        BigDecimal b = new BigDecimal("0.2");
+
+        BigDecimal result = a.add(b);
+
+        System.out.printf("%.20f\n", 0.1 + 0.2);
+        System.out.printf("%.20f", result);
     }
 }
--- src/com/leetcode/solution/difficulty/Easy.java
+++ src/com/leetcode/solution/difficulty/Easy.java
@@ -1421,4 +1421,20 @@ public class Easy {
         }
         return false;
     }
+
+    // 1480. Three Consecutive Odds
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for(int n : arr) {
+            if(n % 2 != 0) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if(count == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
